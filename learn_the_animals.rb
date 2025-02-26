50555015aa0efac64ad27131cde9f74a51baa544
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-02-26 07:19:27 -0500

    Update learn_the_animals.rb

--- learn_the_animals.rb
+++ learn_the_animals.rb
@@ -3,21 +3,9 @@ def learn_animal
   animal_name     = File.read("lib/data/beast/animal_name.txt").strip.to_s
   animal_does     = File.read("lib/data/beast/animal_behaviour.txt").strip.to_s
 
-  new_condition = "
-if 'animal(dog, barks).' == animal_status
-  puts 'The dog is an animal that barks.'
-elsif 'animal(cat, meows).' == animal_status
-  puts 'The cat is an animal that meows.'
-elsif '#{animal_variable}' == animal_status
-  puts 'The #{animal_name} is an animal that #{animal_does}.'
-else
-  puts 'The animal is something else, so I have no idea what it does.'
+  reconstructed_condition = File.read("lib/data/beast/animal_conditional.txt").strip.to_s
 
-  puts 'Lets fine out.'
-
-  learn_animal
-end
-  "
+  new_condition = reconstructed_condition
 
   File.open("animal_learned.rb", "w") { |f|
     f.puts new_condition
